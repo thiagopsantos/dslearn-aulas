46a297ee944d4a22bde3af360b18022fa03ec2c1
Thiago Santos <thiagop.santos182@gmail.com>
2023-05-22 16:36:27 -0400

    feat: exception, validation and security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/components/JwtTokenEnhancer.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dslearnbds.components;
+
+import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class JwtTokenEnhancer implements TokenEnhancer {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken oAuth2AccessToken, OAuth2Authentication oAuth2Authentication) {
+        User user = userRepository.findByEmail(oAuth2Authentication.getName());
+
+        Map<String, Object> map = oAuth2AccessToken.getAdditionalInformation();
+        map.put("userId", user.getId());
+
+        return oAuth2AccessToken;
+    }
+}
